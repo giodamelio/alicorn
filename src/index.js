55422371f022d85471db06282b1a777e29d6993c
Gio d'Amelio <giodamelio@gmail.com>
2015-12-31 20:47:04 -0800

    Express hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(`33 * 100 = ${33 * 100}`);
+import express from 'express';
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+const PORT = 3141;
+app.listen(PORT);
+console.log(`App listening at http://localhost:${PORT}`);
