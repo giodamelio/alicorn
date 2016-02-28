9c56a63c2e6dc169eb62a17e2dbcf32c2640ab4a
Gio d'Amelio <giodamelio@gmail.com>
2016-02-27 18:52:43 -0800

    TicTacToe: Handle some edge cases

--- src/games/tic_tac_toe.js
+++ src/games/tic_tac_toe.js
@@ -1,8 +1,30 @@
 import _ from 'lodash';
 
-export default class TicTacToe {
+import Game from '../game';
+
+export default class TicTacToe extends Game {
   constructor() {
+    super();
+
     this.state = '         '.split('');
+  }
+
+  move(input) {
+    const { index, player } = input;
+    return new Promise((resolve, reject) => {
+      if (index > this.state.length) {
+        return reject('Index must be in the range 0-8');
+      } else if (this.state[index] !== ' ') {
+        return reject('Square already taken');
+      } else if (['X', 'O'].indexOf(player) === -1) {
+        return reject('Player must be X or O');
+      }
+
+      // Make the move
+      this.state[index] = player;
+
+      return resolve(this.state);
+    });
   }
 
   toString() {
