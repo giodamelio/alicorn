b8aad698f8a43134df83818be5735153cc8bd97a
Gio d'Amelio <giodamelio@gmail.com>
2016-03-02 12:54:18 -0800

    TicTacToe: add meta info

--- src/games/tic_tac_toe/index.js
+++ src/games/tic_tac_toe/index.js
@@ -1,2 +1,9 @@
+export const info = {
+  name: 'tic_tac_toe',
+  prettyName: 'Tic Tac Toe',
+  minPlayers: 2,
+  maxPlayers: 2,
+};
+
 export { logic, checkWinner } from './logic';
 export { toString } from './toString';
