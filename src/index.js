a5e2b610cfd6c9d4b33d36006758ef8ff2833600
Gio d'Amelio <giodamelio@gmail.com>
2016-03-02 13:10:17 -0800

    Koa 2.0-alpha hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log(`2 ^ 4 = ${Math.pow(2, 4)}`);
+import Koa from 'koa';
+
+const app = new Koa();
+
+// Handle errors
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.body = {
+      message: err.message,
+    };
+    ctx.status = err.status || 500;
+  }
+});
+
+app.use(async ctx => {
+  ctx.body = 'Hello World!';
+});
+
+console.log('App started at http://localhost:3141/');
+app.listen(3141);
