7ef35ad34c8a14eb1cff8c9c724196e8397e2fe8
Gio d'Amelio <giodamelio@gmail.com>
2016-03-01 19:57:06 -0800

    TicTacToe: Basic stateless version

---
+++ src/games/tic_tac_toe.js
@@ -0,0 +1,117 @@
+import _ from 'lodash';
+
+// Check if a player has won the game
+export function checkWinner(board, player) {
+  // Did the player win any rows
+  if (_(board)
+      .chunk(3)
+      .filter(squares => squares.every(square => square === player))
+      .value()
+      .length > 0) {
+    return true;
+  }
+
+  // Did the player win any columns
+  if (_(board)
+      .chunk(3)
+      .unzip()
+      .filter(squares => squares.every(square => square === player))
+      .value()
+      .length > 0) {
+    return true;
+  }
+
+  // Did the player win either diagonal
+  // Hard code out of lazyness
+  if ((board[0] === player &&
+      board[4] === player &&
+      board[8] === player) || (
+      board[2] === player &&
+      board[4] === player &&
+      board[6] === player)) {
+    return true;
+  }
+
+  return false;
+}
+
+export default function ticTacToe(inputState, move, player) {
+  return new Promise((resolve, reject) => {
+    let state = inputState;
+    /// Validate inputs
+
+    // If the game does not exist create it
+    /* eslint-disable no-else-return */
+    if (!state) {
+      return resolve({
+        status: 'pregame',
+        board: '         '.split(''),
+        nextPlayer: 'X',
+        winner: null,
+      });
+    } else {
+      state = {
+        ...state,
+        status: 'playing',
+      };
+    }
+    /* eslint-enable no-else-return */
+
+    // Check move index
+    if (move < 0 || move > 8) {
+      return reject(new Error('Move must be in range 0-8'));
+    }
+
+    // Check player name
+    if (['X', 'O'].indexOf(player) < 0) {
+      return reject(new Error('Player must be "X" or "O"'));
+    }
+
+    /// Play game
+
+    // Check if it is players turn
+    if (player !== state.nextPlayer) {
+      return reject(new Error('Not your turn'));
+    }
+
+    // Check if square is taken
+    if (state.board[move] !== ' ') {
+      return reject(new Error('square is already taken'));
+    }
+
+    // Make a move
+    const newBoard = state.board.concat();
+    newBoard[move] = state.nextPlayer;
+    state = {
+      ...state,
+      board: newBoard,
+    };
+
+    // Check if the game is done
+    if (checkWinner(state.board, state.nextPlayer)) {
+      return resolve({
+        ...state,
+        status: 'done',
+        winner: 'X',
+      });
+    }
+
+    // Check for a draw
+    if (state.board.indexOf(' ') < 0) {
+      return resolve({
+        ...state,
+        status: 'done',
+        winner: 'draw',
+      });
+    }
+
+    // Update the next player
+    state = {
+      ...state,
+      nextPlayer: state.nextPlayer === 'X' ? 'O' : 'X',
+    };
+
+    // Move done
+    return resolve(state);
+  });
+}
--- test/games/tic_tac_toe_test.js
+++ test/games/tic_tac_toe_test.js
@@ -1,8 +1,177 @@
-import should from 'should';
-
-import TicTacToe from '../../src/games/tic_tac_toe';
+import ticTacToe, { checkWinner } from '../../src/games/tic_tac_toe';
 
 describe('Games', function () {
   describe('TicTacToe', function () {
+    let newGameState;
+    beforeEach(function () {
+      return ticTacToe(null)
+        .then(function (state) {
+          newGameState = state;
+        });
+    });
+
+    describe('validation', function () {
+      it('should create a new game', function () {
+        return ticTacToe(null)
+          .should.finally.deepEqual({
+            status: 'pregame',
+            board: [' ', ' ', ' ',
+                    ' ', ' ', ' ',
+                    ' ', ' ', ' '],
+            nextPlayer: 'X',
+            winner: null,
+          });
+      });
+
+      it('should error on invalid player', function () {
+        return ticTacToe(newGameState, 0, 'Y')
+          .should.be.rejectedWith('Player must be "X" or "O"');
+      });
+
+      it('should error on invlid move index', function () {
+        return ticTacToe(newGameState, 10, 'X')
+          .should.be.rejectedWith('Move must be in range 0-8');
+      });
+    });
+
+    describe('game', function () {
+      it('should fail if it is not the players turn', function () {
+        return ticTacToe(newGameState, 0, 'O')
+          .should.be.rejectedWith('Not your turn');
+      });
+
+      it('should fail move if square is already taken', function () {
+        return ticTacToe({
+          ...newGameState,
+          board: ['X', ' ', ' ',
+                  ' ', ' ', ' ',
+                  ' ', ' ', ' '],
+        }, 0, 'X')
+          .should.be.rejectedWith('square is already taken');
+      });
+
+      it('should allow valid move', function () {
+        return ticTacToe(newGameState, 0, 'X')
+          .should.eventually.deepEqual({
+            ...newGameState,
+            board: ['X', ' ', ' ',
+                    ' ', ' ', ' ',
+                    ' ', ' ', ' '],
+            status: 'playing',
+            nextPlayer: 'O',
+          });
+      });
+
+      it('should end game when a player has won', function () {
+        return ticTacToe({
+          ...newGameState,
+          board: ['X', 'X', ' ',
+                  ' ', ' ', ' ',
+                  ' ', ' ', ' '],
+          nextPlayer: 'X',
+        }, 2, 'X').should.eventually.deepEqual({
+          ...newGameState,
+          board: ['X', 'X', 'X',
+                  ' ', ' ', ' ',
+                  ' ', ' ', ' '],
+          status: 'done',
+          nextPlayer: 'X',
+          winner: 'X',
+        });
+      });
+
+      it('should handle draw', function () {
+        return ticTacToe({
+          ...newGameState,
+          board: ['X', 'O', 'X',
+                  'O', 'X', 'X',
+                  'O', ' ', 'O'],
+        }, 7, 'X').should.eventually.deepEqual({
+          ...newGameState,
+          board: ['X', 'O', 'X',
+                  'O', 'X', 'X',
+                  'O', 'X', 'O'],
+          status: 'done',
+          nextPlayer: 'X',
+          winner: 'draw',
+        });
+      });
+    });
+
+    describe('checkWinner', function () {
+      it('won with a row', function () {
+        const boards = [
+          ['X', 'X', 'X',
+           ' ', ' ', ' ',
+           ' ', ' ', ' '],
+          [' ', ' ', ' ',
+           'X', 'X', 'X',
+           ' ', ' ', ' '],
+          [' ', ' ', ' ',
+           ' ', ' ', ' ',
+           'X', 'X', 'X'],
+        ];
+        Promise.all(boards.map(function (board) {
+          return checkWinner(
+            board,
+            'X'
+          ).should.equal(true);
+        }));
+      });
+
+      it('won with a column', function () {
+        const boards = [
+          ['X', ' ', ' ',
+           'X', ' ', ' ',
+           'X', ' ', ' '],
+          [' ', 'X', ' ',
+           ' ', 'X', ' ',
+           ' ', 'X', ' '],
+          [' ', ' ', 'X',
+           ' ', ' ', 'X',
+           ' ', ' ', 'X'],
+        ];
+        Promise.all(boards.map(function (board) {
+          return checkWinner(
+            board,
+            'X'
+          ).should.equal(true);
+        }));
+      });
+
+      it('won with a diagonal', function () {
+        const boards = [
+          ['X', ' ', ' ',
+           ' ', 'X', ' ',
+           ' ', ' ', 'X'],
+          [' ', ' ', 'X',
+           ' ', 'X', ' ',
+           'X', ' ', ' '],
+        ];
+        Promise.all(boards.map(function (board) {
+          return checkWinner(
+            board,
+            'X'
+          ).should.equal(true);
+        }));
+      });
+
+      it('no winner', function () {
+        const boards = [
+          ['X', ' ', ' ',
+           ' ', 'X', ' ',
+           ' ', ' ', 'O'],
+          [' ', ' ', 'X',
+           ' ', 'O', ' ',
+           'X', ' ', ' '],
+        ];
+        Promise.all(boards.map(function (board) {
+          return checkWinner(
+            board,
+            'X'
+          ).should.equal(false);
+        }));
+      });
+    });
   });
 });
