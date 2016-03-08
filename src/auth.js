4de0cef07f15e996fcb5528b40689b9a1f4063f7
Gio d'Amelio <giodamelio@gmail.com>
2016-03-07 19:29:52 -0800

    Basic session store with leveldb

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,30 @@
+import level from 'level';
+
 import config from './config';
+
+const sessionStore = level(config.sessionStorePath);
 
 export default function (server) {
   // Setup the authentication
   server.auth.strategy('users', 'jwt', {
     key: config.jwtSecret,
     validateFunc(decoded, request, callback) {
-      callback(null, true);
+      if (!decoded.id) {
+        callback(null, false);
+      }
+
+      // Check to see if the session exists
+      sessionStore.get(decoded.id, (err) => {
+        if (err) {
+          if (err.type === 'NotFoundError') {
+            callback(null, false);
+          }
+
+          callback(err, false);
+        }
+
+        callback(null, true);
+      });
     },
     verifyOptions: {
       algorithms: [
--- src/config.js
+++ src/config.js
@@ -3,7 +3,12 @@ import rc from 'rc';
 import { name } from '../package.json';
 
 const config = rc(name, {
+  // The secret for our sessions. Genereate with
+  // node -e "console.log(require('crypto').randomBytes(32).toString('hex'));"
   jwtSecret: null,
+
+  // The path to the session store
+  sessionStorePath: 'session-db/',
 });
 
 if (!config.jwtSecret) {
