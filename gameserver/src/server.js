a0666821436903417a758ac35b1965d58cfbd61a
Gio d'Amelio <giodamelio@gmail.com>
2016-04-13 09:38:29 -0700

    Add koa-router

--- gameserver/src/server.js
+++ gameserver/src/server.js
@@ -1,11 +1,18 @@
 import Koa from 'koa';
+import KoaRouter from 'koa-router';
 
 import logger from './logger';
 
 const app = new Koa();
+const router = new KoaRouter();
 
-app.use(async (ctx) => {
+// Create our routes
+router.get('/', async (ctx) => {
   ctx.body = 'Hello World!';
 });
+
+// Register routes
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 export default app;
