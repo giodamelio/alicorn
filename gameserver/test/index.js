d64c39d7477e1c03961ae143a21c50327c5f0ca1
Gio d'Amelio <giodamelio@gmail.com>
2016-04-12 22:47:48 -0700

    Test koa server

--- gameserver/test/index.js
+++ gameserver/test/index.js
@@ -1,1 +1,12 @@
+import supertest from 'supertest-as-promised';
+
 import gameserver from '../src/server';
+
+describe('test', () => {
+  it('should return hello world!', () => (
+    supertest(gameserver.listen())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!')
+  ));
+});
